ecda98efab28c084dd48f76bd2d953805a17682d
Cherry <cherry@sparklet.org>
2023-12-23 03:58:49 -0500

    fade works

--- volatility.py
+++ volatility.py
@@ -13,11 +13,7 @@ SPILL_RARITY = 500
 
 
 def colored(text, color):
-    print(f"\x1b[38;5;{color}m{text}\x1b[0m")
-
-
-# Example usage
-fade_effect("Hello, Fading World!")
+    return f"\x1b[38;5;{color}m{text}\x1b[0m"
 
 
 def roll_chance(chance: int) -> bool:
@@ -29,30 +25,39 @@ def spill_effect(input_string: str) -> str:
     width = max([len(x) for x in lines])
     lines = [x.ljust(width) for x in lines]
 
-    # key: column, value: stuck character
+    # Stuck column => Line number to copy from
     stuck = {}
 
     # stuck chars fade to black, so this calculates
     # how many rows ago the char was stuck
-    def get_color(row: int) -> int:
-        ago = len(lines) - stuck[column]
+    def get_color(row: int, col: int) -> int:
+        stuck_start = stuck.get(col)
+
+        if stuck_start is None:
+            return WHITE
+
+        ago = row - stuck_start
+        return FADE_COLORS[min(ago, len(FADE_COLORS) - 1)]
 
     res = []
 
-    for line in lines:
-        for ch in line:
+    for lineno, line in enumerate(lines):
+        for i, ch in enumerate(line):
             if ch.isspace():
                 continue
 
             if roll_chance(SPILL_RARITY):
-                stuck[line.index(ch)] = ch
+                stuck[i] = lineno
 
         def process_char(col: int) -> str:
             current_ch = line[col]
             if not current_ch.isspace():
+                # TODO color
                 return current_ch
 
-            return stuck.get(col, current_ch)
+            st = stuck.get(col)
+            ch_to_print = lines[st][col] if st else current_ch
+            return colored(ch_to_print, get_color(lineno, col))
 
         glitched_line = [process_char(col) for col in range(width)]
         res.append("".join(glitched_line))

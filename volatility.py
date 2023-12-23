d952a9cde701b3b6a72e11dd98742e41937ee90f
Cherry <cherry@sparklet.org>
2023-12-23 03:15:15 -0500

    spill (no rand)

--- volatility.py
+++ volatility.py
@@ -1,15 +1,27 @@
-import sys, inspect, os
+import sys, inspect, random
+
+# , os
 
 
-#    _   ______  __   ___ ____________   ____________  __
-#   | | / / __ \/ /  / _ /_  __/  _/ /  /  _/_  __/\ \/ /
-#   | |/ / /_/ / /__/ __ |/ / _/ // /___/ /  / /    \  /
-#   |___/\____/____/_/ |_/_/ /___/____/___/ /_/     /_/
-#
+def spill(input_string: str):
+    lines = input_string.split("\n")
+    width = max([len(x) for x in lines])
+    lines = [x.ljust(width) for x in lines]
+
+    stuck_rows = [None for x in range(width)]
+
+    res = []
+
+    for line in lines:
+        glitched_line = [stuck_rows[ch] or line[ch] for ch in range(width)]
+        res.append("".join(glitched_line))
+
+    return "\n".join(res)
 
 
 frame = inspect.currentframe() or sys.exit()
 quine = inspect.getsource(frame)
+quine = spill(quine)
 print(quine, end="")
 
-os.remove(__file__)
+# os.remove(__file__)

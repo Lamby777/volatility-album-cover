fbc0cc135f0fdd3dd0038a012ae71cf5c498f5ee
Cherry <cherry@sparklet.org>
2023-12-23 21:00:51 -0500

    one-line `if` statements

--- volatility.py
+++ volatility.py
@@ -10,21 +10,17 @@ def spill_effect(input_string: str) -> str:
     stuck = {}
     def get_color(row: int, col: int) -> int:
         stuck_start = stuck.get(col)
-        if stuck_start is None:
-            return WHITE
+        if stuck_start is None: return WHITE
         ago = row - stuck_start
         return FADE_COLORS[min(ago, len(FADE_COLORS) - 1)]
     res = []
     for lineno, line in enumerate(lines):
         for i, ch in enumerate(line):
-            if ch.isspace():
-                continue
-            if roll_chance(SPILL_RARITY):
-                stuck[i] = lineno
+            if ch.isspace(): continue
+            if roll_chance(SPILL_RARITY): stuck[i] = lineno
         def process_char(col: int) -> str:
             current_ch = line[col]
-            if not current_ch.isspace():
-                return colored(current_ch, CODE_COLOR)
+            if not current_ch.isspace(): return colored(current_ch, CODE_COLOR)
             st = stuck.get(col)
             ch_to_print = lines[st][col] if st else current_ch
             return colored(ch_to_print, get_color(lineno, col))
